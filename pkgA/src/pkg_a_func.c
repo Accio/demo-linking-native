714ca7084d1efa715c7d92cb3df7d8c49fb89374
Jitao David Zhang <jitao_david.zhang@roche.com>
2020-01-04 06:56:50 +0100

    use a dummy function that runs in C, and a function that returns SEXP, callable from R

--- pkgA/src/pkg_a_func.c
+++ pkgA/src/pkg_a_func.c
@@ -5,7 +5,15 @@
 
 #include "pkg_a.h"
 
-void pkgA_func() {
-  Rprintf("This is a function from pkgA");
-  return;
+int pkgA_version() {
+  return 1;
+}
+
+SEXP pkgA_func() {
+  SEXP res = PROTECT(allocVector(INTSXP, 1));
+  int v = pkgA_version();
+  Rprintf("This is a function from pkgA version %d\n", v);
+  INTEGER(res)[0]=v;
+  UNPROTECT(1);
+  return res;
 }
