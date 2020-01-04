4fe54a6dde837d7491f201e0ea6d181cde8c97ab
Jitao David Zhang <jitao_david.zhang@roche.com>
2020-01-04 08:08:05 +0100

    pass mkChar(char *) as SEXP to pkgA_func

--- pkgB/src/pkg_b_func.c
+++ pkgB/src/pkg_b_func.c
@@ -3,7 +3,7 @@
 SEXP pkgB_func() {
    Rprintf("Internally, pkgB calls function from pkgA.");
    Rprintf("Installed pkgA version: %d", pkgA_version());
-   pkgA_func("pkgB");
+   pkgA_func(mkChar("pkgB"));
    return R_NilValue;
 }
 
