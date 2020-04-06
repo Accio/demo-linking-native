ec685b609763c15d10c97df110b7468dd253e729
Jitao David Zhang <jitao_david.zhang@roche.com>
2020-04-06 13:27:23 +0200

    renamed as pkgIn, and make check returns no error or warning

--- pkgIn/inst/include/pkgIn.h
+++ pkgIn/inst/include/pkgIn.h
@@ -1,6 +1,8 @@
 #include <R.h>
 #include <Rinternals.h>
 
+// functions imported from pkgOut
+// They are cast from DL_FUNC to the correct return types (SEXP, int)
 SEXP(*pkgOut_func)(SEXP);
 int(*pkgOut_version)();
 
