f964fa5622374965181f0334f49051b0009febf3
Jitao David Zhang <jitao_david.zhang@roche.com>
2020-08-14 15:50:49 +0200

    declare variables as extern, since they are defined elsewhere in init.c

--- pkgIn/inst/include/pkgIn.h
+++ pkgIn/inst/include/pkgIn.h
@@ -1,9 +1,15 @@
 #include <R.h>
 #include <Rinternals.h>
 
+#ifndef _pkgIn
+#define _pkgIn
+
 // functions imported from pkgOut
 // They are cast from DL_FUNC to the correct return types (SEXP, int)
 extern SEXP(*pkgOut_func)(SEXP);
 extern int(*pkgOut_version)();
 
-SEXP pkgIn_func();
+extern SEXP pkgIn_func();
+
+#endif
+
